c9b5461b6e58a0c76d156eb456a503aa3aeae8e0
arithesage <ariaththesage@gmail.com>
2024-06-25 10:52:53 +0200

    OSHelpers is now 'singletonized'. Adding functions and permissions for notification showing.

--- app/src/main/java/me/arithesage/java/android/libs/helpers/testapp/MainActivity.java
+++ app/src/main/java/me/arithesage/java/android/libs/helpers/testapp/MainActivity.java
@@ -13,7 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DialogHelpers.Init (this);
-        DialogHelpers.Get().ShowMessage ("Hello!");
+        OSHelpers.Init (this);
+
+        if (OSHelpers.Initialized()) {
+            OSHelpers.Get().ShowNotification ("TestApp", "Hello!");
+        }
     }
 }
--- helpers/src/main/java/me/arithesage/java/android/libs/helpers/OSHelpers.java
+++ helpers/src/main/java/me/arithesage/java/android/libs/helpers/OSHelpers.java
@@ -1,9 +1,51 @@
 package me.arithesage.java.android.libs.helpers;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.AsyncTask;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 
 public class OSHelpers {
+    private static OSHelpers instance = null;
+    private Context appContext = null;
+
+
+    /**
+     * Returns the singleton
+     */
+    public static OSHelpers Get() {
+        return instance;
+    }
+
+
+    /**
+     * Initializes the singleton with the application context.
+     *
+     * We could simply pass context every time we call a function, but
+     * i personally prefer this way.
+     *
+     * @param appContext The application context used by all functionss.
+     */
+    public static void Init (Context appContext) {
+        if ((instance == null) && (appContext != null)) {
+            instance = new OSHelpers ();
+            instance.appContext = appContext;
+        }
+    }
+
+
+    /**
+     * Returns if the singleton has been initialized
+     */
+    public static boolean Initialized () {
+        return (OSHelpers.instance != null);
+    }
+
+
     /**
      * Class made to provide a way to easily running debuggable async
      * tasks in Android.
@@ -39,7 +81,40 @@ public class OSHelpers {
      * @param task Task to perform
      */
     @SuppressWarnings("deprecation")
-    public static void RunAsync (AndroidAsyncTask task) {
+    public void RunAsync (AndroidAsyncTask task) {
         AsyncTask.execute (task);
     }
+
+
+    /**
+     * Shows a notification message
+     *
+     * @param context NOTE: Here "context" is a way to refer to the
+     *                notification title, not the application context.
+     * @param message
+     */
+    public void ShowNotification (String context, String message) {
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder (appContext);
+
+        notificationBuilder.setContentTitle (context);
+        notificationBuilder.setContentText (message);
+        notificationBuilder.setAutoCancel (true);
+
+        Notification notification = notificationBuilder.build();
+
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from (appContext);
+
+        int notificationId = new java.util.Random().nextInt();
+        notificationManager.notify (notificationId, notification);
+    }
 }
+
+
+
+
+
+
+
+
